6a449f2ea11eecfa28eb59525be34986cbbe44e9
projectwakii <projectwakii@gmail.com>
2015-06-17 22:34:58 +0900

    Added IBOutlets, but not actions.

---
+++ ShotsApp2/Home.swift
@@ -0,0 +1,62 @@
+//
+//  Home.swift
+//  ShotsApp2
+//
+//  Created by David Liu on 17/06/2015.
+//  Copyright © 2015 David Liu. All rights reserved.
+//
+
+import UIKit
+
+class Home: UIViewController {
+    
+    @IBOutlet weak var userButton: UIButton!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    
+    @IBOutlet weak var backgroundMaskView: UIView!
+    
+    @IBOutlet weak var dialogView: UIView!
+    @IBOutlet weak var imageButton: UIButton!
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var favoritesLabel: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var shareButton: UIButton!
+    
+    
+    
+    
+    
+    
+    @IBOutlet weak var popoverView: UIView!
+    @IBOutlet weak var shareView: UIView!
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
